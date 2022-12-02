7c0b197c490e20236f13a90fdfd2b3fac073c2e0
Alex  Morte <alexandersonbm@hotmail.com>
2022-12-02 06:22:07 -0300

    feat: update uart lib

--- rpi/uart/uart.c
+++ rpi/uart/uart.c
@@ -6,9 +6,7 @@
 #include <wiringPi.h>
 #include <wiringSerial.h>
 
-extern void init();
-extern void write_char(char ch);
-extern void clear_display();
+
 
 #define BAUD_RATE 9600
 
@@ -26,10 +24,19 @@ int init_uart(){
         return 0;
     }
 
-    if (wiringPiSetup() == -1) {
-        fprintf(stdout, "Unable to start wiringPi: %s\n", strerror(errno));
-        return 0;
+    printf("Reading secret code\n");
+    
+    // Unlock
+    int i = 0;
+    while(i < 6){
+        if (serialDataAvail(serial_port)){
+            char byte = serialGetchar(serial_port); /* receive character serially*/
+            printf("%d %c\n",byte, byte);
+            i = byte == key[i] ? i + 1 : 0;
+            fflush(stdout);
+        }
     }
+    printf("Secret code found\n");
     return serial_port;
 }
 
@@ -38,63 +45,25 @@ int init_uart(){
  */
 int busy_wait(int serial_port){
     int t = 0;
-    while(!serialDataAvail(serial_port) && t < 1e9) t++; 
-    if(t >= 1e9) {
+    while(!serialDataAvail(serial_port) && t < 10) {
+        delay(90); 
+        t++;
+    }
+    if(t >= 10) {
         printf("Timeout...\n");
         return 0;
     }
     return 1;
 }
 
-
-
-int main()
-{
-    printf("Initializing Display...\n");
-    init();
-    printf("Initializing Uart...\n");
-    int serial_port = init_uart();
-    if(!serial_port) return 1;
-    printf("Uart initialized\n");
-    printf("Reading secret code\n");
-    // Unlock
-    int i = 0;
-    while(i < 6){
-        if (serialDataAvail(serial_port)){
-            char byte = serialGetchar(serial_port); /* receive character serially*/
-            i = byte == key[i] ? i + 1 : 0;
-            fflush(stdout);
-        }
-    }
-    printf("Secret code found\n");
- 
-    while(1){
-        printf("Digite um comando: ");
-        int cmd;
-        scanf("%d", &cmd);
-        clear_display();
-        char byte = cmd;
-        serialPutchar(serial_port, byte);
-        int response = busy_wait(serial_port);
-        if(!response) continue;
-        clear_display();
-        
-        char*msg = "sensor ";
-        for(int i=0;i<7;i++) write_char(msg[i]);
-        write_char('0' + (cmd>>3)/10);
-        write_char('0' + (cmd>>3)%10);
-        for(int i=0;i<31;i++) write_char(' ');
-        msg = "valor ";
-        for(int i=0;i<6;i++) write_char(msg[i]);
-
-        while(serialDataAvail(serial_port)){
-            char byte = serialGetchar(serial_port);
-            if(byte >= '0' && byte <= '9')
-                write_char(byte);
-            if(byte >= 0 && byte <= 9)
-                write_char(byte+'0');
-            printf("Response: %d\n", byte);
-        }
-      
-    }
+void send_data(int port, int data){
+    serialPutchar(port, data);
 }
+
+int get_response(int port){
+    
+    int response = busy_wait(port);
+    if(!response) return -1;
+    return serialGetchar(port);
+}
+
